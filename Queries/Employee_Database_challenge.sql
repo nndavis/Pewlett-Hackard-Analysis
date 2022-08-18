d2d73635bd1368a558dd5bcdb41ce5145a6bdbfa
NoahKoala <104074135+nndavis@users.noreply.github.com>
2022-08-18 14:20:10 -0500

    challenge sql update
    I had my code commented out, reverted back.

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -9,23 +9,23 @@ FROM titles;
 -- Step 3-5: create new table , join tables on primary key, filter birth date 1952-1955,
 -- order by employee number
 
--- Select e.emp_no, e.first_name, e.last_name, ti.title, ti.from_date, ti.to_date
--- INTO retirement_titles
--- FROM employees as e
--- INNER JOIN titles as ti on (e.emp_no=ti.emp_no)
--- WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
--- ORDER BY e.emp_no;
+Select e.emp_no, e.first_name, e.last_name, ti.title, ti.from_date, ti.to_date
+INTO retirement_titles
+FROM employees as e
+INNER JOIN titles as ti on (e.emp_no=ti.emp_no)
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+ORDER BY e.emp_no;
 
--- Starter code below
--- Use Dictinct with Orderby to remove duplicate rows
--- SELECT DISTINCT ON (emp_no) emp_no,
--- first_name,
--- last_name,
--- title
--- INTO unique_titles
--- FROM retirement_titles
--- WHERE to_date = '9999-01-01'
--- ORDER BY emp_no, to_date DESC;
+--Starter code below
+--Use Dictinct with Orderby to remove duplicate rows
+SELECT DISTINCT ON (emp_no) emp_no,
+first_name,
+last_name,
+title
+INTO unique_titles
+FROM retirement_titles
+WHERE to_date = '9999-01-01'
+ORDER BY emp_no, to_date DESC;
 
 
 -- Checking unique_titles
@@ -57,9 +57,9 @@ FROM titles;
 
 --Step 4 Use a DISTINCT ON statement to retrieve the first occurrence of the employee number 
 --for each set of rows defined by the ON () clause.
--- SELECT DISTINCT ON (ti.emp_no) emp_no, title
--- FROM titles as ti
--- ORDER BY ti.emp_no, to_date DESC;
+SELECT DISTINCT ON (ti.emp_no) emp_no, title
+FROM titles as ti
+ORDER BY ti.emp_no, to_date DESC;
 
 SELECT distinct on (ti.emp_no) ti.emp_no,
 emp.first_name, emp.last_name, emp.birth_date,
